6b03ac44e5174e0d5f04de183c32d2616262d606
Captoweb <captainphp@yandex.ru>
2017-07-15 23:23:00 +0300

    Delete menu.php

--- menu.php
+++
@@ -1,8 +0,0 @@
-<ul>
-    <li><a href="/">Главная</a></li>
-    <li><a href="https://ru.hexlet.io/my""#">Сайт по которому я готовился</a></li>
-    <li><a href="/images/books.jpg""#">Книги которые я прочитал</a></li>
-        <li><a href="/images/programmer.jpg""#">Как я отдыхаю</a></li>
-    <li><a href="/images/database.jpg""#">Самый дешевый хостинг</a></li>
-    <li><a href="/images/Git_begining.jpg""#">Git: начало</a></li>
-</ul>
